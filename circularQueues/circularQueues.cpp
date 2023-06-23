68813a8b914aa38b288c7235e1f92c63d8f6203c
RamaRamdani03 <ramaramdani449@gamil.com>
2023-06-23 15:04:52 +0700

    Jika rear berada di posisi terakhir array maka kembali ke awal array

--- circularQueues/circularQueues.cpp
+++ circularQueues/circularQueues.cpp
@@ -27,3 +27,8 @@ public:
 		if (FRONT == -1) {
 			FRONT = 0;
 			REAR = 0;
+		}
+		else {
+			// Jika REAR berada diposisi terakhir array, kembali ke awal array
+			if (REAR == max - 1)
+				REAR = 0;
